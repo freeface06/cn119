a8f277d78c946d0fd1e0baf94a6a937f1c2c6109
이호정 <freeface06@naver.com>
2025-07-02 15:01:23 +0900

    init

---
+++ data.js
@@ -0,0 +1,363 @@
+const DATA = [
+    {
+        "title": "제32기 소방위 관리역량 과정",
+        "author": "오진웅",
+        "created_date": "2025-07-01",
+        "type": "기본교육",
+        "residential": "합숙",
+        "total_participants": "38명",
+        "participants_detail": {
+            "daejeon": 4,
+            "sejong": 3,
+            "chungbuk": 12,
+            "chungnam": 18,
+            "fire_agency": 1
+        },
+        "start_date": "2025-07-09",
+        "end_date": "2025-07-11",
+        "file_name": "제32기+소방위+관리역량+과정+입교+및+생활안내.pdf",
+        "file_path": "./file/제32기+소방위+관리역량+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제31기 소방위 관리역량 과정",
+        "author": "오진웅",
+        "created_date": "2025-07-01",
+        "type": "기본교육",
+        "residential": "합숙",
+        "total_participants": "38명",
+        "participants_detail": {
+            "daejeon": 7,
+            "sejong": 2,
+            "chungbuk": 12,
+            "chungnam": 17
+        },
+        "start_date": "2025-07-07",
+        "end_date": "2025-07-09",
+        "file_name": "제31기 소방위 관리역량 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제31기+소방위+관리역량+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제4기 소방행정실무 과정",
+        "author": "오진웅",
+        "created_date": "2025-07-01",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "27명",
+        "participants_detail": {
+            "daejeon": 2,
+            "sejong": 2,
+            "chungbuk": 9,
+            "chungnam": 14
+        },
+        "start_date": "2025-07-07",
+        "end_date": "2025-07-09",
+        "file_name": "제4기 소방행정실무 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제4기+소방행정실무+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제4기 예산회계실무 과정",
+        "author": "오진웅",
+        "created_date": "2025-07-01",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "26명",
+        "participants_detail": {
+            "daejeon": 3,
+            "sejong": 1,
+            "chungbuk": 8,
+            "chungnam": 14
+        },
+        "start_date": "2025-07-02",
+        "end_date": "2025-07-04",
+        "file_name": "제4기 예산회계실무 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제4기+예산회계실무+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제7기 급류구조 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-19",
+        "type": "전문교육",
+        "residential": "외부합숙",
+        "total_participants": "20명",
+        "participants_detail": {
+            "daejeon": 4,
+            "sejong": 1,
+            "chungbuk": 6,
+            "chungnam": 9
+        },
+        "start_date": "2025-06-23",
+        "end_date": "2025-06-27",
+        "file_name": "제7기 급류구조과정 교육 운영계획.pdf",
+        "file_path": "./file/제7기+급류구조과정+교육+운영계획.pdf"
+    },
+    {
+        "title": "제3기 펌뷸런스대원 기초능력향상 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-19",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "24명",
+        "participants_detail": {
+            "daejeon": 2,
+            "sejong": 1,
+            "chungbuk": 8,
+            "chungnam": 13
+        },
+        "start_date": "2025-06-23",
+        "end_date": "2025-06-24",
+        "file_name": "제3기 펌뷸런스대원 기초능력향상 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제3기+펌뷸런스대원+기초능력향상+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제5기 재난현장 언론대응 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-19",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "17명",
+        "participants_detail": {
+            "daejeon": 1,
+            "chungbuk": 3,
+            "chungnam": 13
+        },
+        "start_date": "2025-06-23",
+        "end_date": "2025-06-25",
+        "file_name": "제5기 재난현장 언론대응 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제5기+재난현장+언론대응+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제30기 소방위 관리역량 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-19",
+        "type": "기본교육",
+        "residential": "합숙",
+        "total_participants": "38명",
+        "participants_detail": {
+            "daejeon": 7,
+            "sejong": 3,
+            "chungbuk": 12,
+            "chungnam": 14,
+            "fire_agency": 2
+        },
+        "start_date": "2025-06-18",
+        "end_date": "2025-06-20",
+        "file_name": "제30기 소방위 관리역량 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제30기+소방위+관리역량+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제4기 위험물 실무 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-19",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "30명",
+        "participants_detail": {
+            "daejeon": 6,
+            "sejong": 2,
+            "chungbuk": 8,
+            "chungnam": 14
+        },
+        "start_date": "2025-06-16",
+        "end_date": "2025-06-20",
+        "file_name": "제4기 위험물 실무 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제4기+위험물+실무+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제3기 초급 현장지휘관 자격인증 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-19",
+        "type": "전문교육",
+        "residential": "비합숙",
+        "total_participants": "13명",
+        "participants_detail": {
+            "sejong": 1,
+            "chungbuk": 6,
+            "chungnam": 6
+        },
+        "start_date": "2025-06-16",
+        "end_date": "2025-06-20",
+        "file_name": "제3기 초급 현장지휘관 자격인증 과정 교육 운영계획.pdf",
+        "file_path": "./file/제3기+초급+현장지휘관+자격인증+과정+교육+운영계획.pdf"
+    },
+    {
+        "title": "제29기 소방위 관리역량 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-19",
+        "type": "기본교육",
+        "residential": "합숙",
+        "total_participants": "36명",
+        "participants_detail": {
+            "daejeon": 2,
+            "sejong": 2,
+            "chungbuk": 12,
+            "chungnam": 19,
+            "fire_agency": 1
+        },
+        "start_date": "2025-06-16",
+        "end_date": "2025-06-18",
+        "file_name": "제29기 소방위 관리역량 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제29기+소방위+관리역량+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제28기 소방위 관리역량 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-10",
+        "type": "기본교육",
+        "residential": "합숙",
+        "total_participants": "36명",
+        "participants_detail": {
+            "daejeon": 2,
+            "sejong": 3,
+            "chungbuk": 12,
+            "chungnam": 17,
+            "fire_agency": 2
+        },
+        "start_date": "2025-06-11",
+        "end_date": "2025-06-13",
+        "file_name": "제28기 소방위 관리역량 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제28기+소방위+관리역량+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제22기 소방드론 자격취득 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-10",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "13명",
+        "participants_detail": {
+            "daejeon": 2,
+            "sejong": 1,
+            "chungbuk": 3,
+            "chungnam": 6,
+            "fire_agency": 1
+        },
+        "start_date": "2025-06-09",
+        "end_date": "2025-07-11",
+        "file_name": "제22기 소방드론 자격취득 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제22기+소방드론+자격취득+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제2기 초급 현장지휘관 자격인증 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-10",
+        "type": "전문교육",
+        "residential": "비합숙",
+        "total_participants": "18명",
+        "participants_detail": {
+            "daejeon": 2,
+            "sejong": 1,
+            "chungbuk": 6,
+            "chungnam": 9
+        },
+        "start_date": "2025-06-09",
+        "end_date": "2025-06-13",
+        "file_name": "제2기 초급 현장지휘관 자격인증과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제2기+초급+현장지휘관+자격인증과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제2기 전문구급소생술(시뮬레이션) 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-10",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "22명",
+        "participants_detail": {
+            "daejeon": 4,
+            "chungbuk": 7,
+            "chungnam": 11
+        },
+        "start_date": "2025-06-09",
+        "end_date": "2025-06-13",
+        "file_name": "제2기 전문구급소생술(시뮬레이션) 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제2기+전문구급소생술(시뮬레이션)+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제27기 소방위 관리역량 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-10",
+        "type": "기본교육",
+        "residential": "합숙",
+        "total_participants": "37명",
+        "participants_detail": {
+            "daejeon": 5,
+            "sejong": 2,
+            "chungbuk": 12,
+            "chungnam": 17,
+            "fire_agency": 1
+        },
+        "start_date": "2025-06-09",
+        "end_date": "2025-06-11",
+        "file_name": "제27기 소방위 관리역량 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제27기+소방위+관리역량+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제2기 소방시설 기초(기계) 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-04",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "26명",
+        "participants_detail": {
+            "daejeon": 2,
+            "sejong": 1,
+            "chungbuk": 9,
+            "chungnam": 14
+        },
+        "start_date": "2025-06-02",
+        "end_date": "2025-06-05",
+        "file_name": "제2기 소방시설기초(기계) 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제2기+소방시설기초(기계)+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제2기 소방시설 기초(전기) 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-04",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "27명",
+        "participants_detail": {
+            "daejeon": 3,
+            "sejong": 2,
+            "chungbuk": 8,
+            "chungnam": 14
+        },
+        "start_date": "2025-06-02",
+        "end_date": "2025-06-05",
+        "file_name": "제2기 소방시설기초(전기) 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제2기+소방시설기초(전기)+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제2기 다수사상자 구급대응 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-04",
+        "type": "전문교육",
+        "residential": "합숙",
+        "total_participants": "17명",
+        "participants_detail": {
+            "daejeon": 1,
+            "chungbuk": 7,
+            "chungnam": 9
+        },
+        "start_date": "2025-06-02",
+        "end_date": "2025-06-05",
+        "file_name": "제2기 다수사상자 구급대응 과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제2기+다수사상자+구급대응+과정+입교+및+생활안내.pdf"
+    },
+    {
+        "title": "제2기 현장지휘 교육 교관 양성 과정",
+        "author": "오진웅",
+        "created_date": "2025-06-04",
+        "type": "전문교육",
+        "residential": "비합숙",
+        "total_participants": "17명",
+        "participants_detail": {
+            "daejeon": 2,
+            "chungbuk": 6,
+            "chungnam": 9
+        },
+        "start_date": "2025-06-02",
+        "end_date": "2025-06-05",
+        "file_name": "제1기 현장지휘교육 교관 양성과정 입교 및 생활안내.pdf",
+        "file_path": "./file/제1기+현장지휘교육+교관+양성과정+입교+및+생활안내.pdf"
+    }
+]
---
+++ main.js
@@ -0,0 +1,155 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const calendarEl = document.getElementById('calendar');
+
+    const titleCategoryMap = [
+        { keyword: '관리역량', category: '소방행정', icon: 'fire_extinguisher' },
+        { keyword: '소방시설', category: '시설기술', icon: 'engineering' },
+        { keyword: '예산회계', category: '행정/회계', icon: 'account_balance' },
+        { keyword: '위험물', category: '화학/위험', icon: 'science' },
+        { keyword: '급류구조', category: '구조', icon: 'flood' },
+        { keyword: '재난현장 언론', category: '재난대응', icon: 'campaign' },
+        { keyword: '펌블런스', category: '구급', icon: 'medical_services' },
+        { keyword: '전문구급소생술', category: '구급', icon: 'monitor_heart' },
+        { keyword: '다수사상자', category: '구급', icon: 'group' },
+        { keyword: '소방드론', category: '드론', icon: 'flight' },
+        { keyword: '현장지휘관', category: '지휘', icon: 'assignment' },
+        { keyword: '교관', category: '교육훈련', icon: 'school' }
+    ];
+
+    function getIconForTitle(title) {
+        const match = titleCategoryMap.find(item => title.includes(item.keyword));
+        return match ? match.icon : '';
+    }
+
+    const events = DATA.map(item => ({
+        title: item.title,
+        start: item.start_date,
+        end: dayjs(item.end_date).add(1, 'day').format('YYYY-MM-DD'),
+        extendedProps: {
+            icon: getIconForTitle(item.title),
+            author: item.author,
+            type: item.type,
+            residential: item.residential,
+            participants: item.total_participants,
+            participants_detail: item.participants_detail,
+            file: item.file_path,
+            created_date: item.created_date
+        },
+        classNames: getEventClassByType(item.type)
+    }));
+
+    function getEventClassByType(type) {
+        switch (type) {
+            case '기본교육':
+                return ['fc-type-basic'];
+            case '전문교육':
+                return ['fc-type-expert'];
+            default:
+                return ['fc-type-default'];
+        }
+    }
+
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        locale: 'ko',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: ''
+        },
+        buttonText: {
+            today: '오늘'
+        },
+        events: events,
+        contentHeight: 'auto',
+        expandRows: true,
+        dayMaxEventRows: false,
+        eventClick: function (info) {
+            const props = info.event.extendedProps;
+
+            document.getElementById('modalTitle').textContent = info.event.title;
+            document.getElementById('modalAuthor').textContent = props.author;
+            document.getElementById('modalCreated').textContent = props.created_date || 'N/A';
+            document.getElementById('modalType').textContent = props.type;
+            document.getElementById('modalResidential').textContent = props.residential;
+
+            const participantsText = formatParticipants(props.participants, props.participants_detail);
+            document.getElementById('modalParticipants').textContent = participantsText;
+
+            document.getElementById('modalStartDate').textContent = info.event.startStr;
+            document.getElementById('modalEndDate').textContent = dayjs(info.event.endStr).subtract(1, 'day').format('YYYY-MM-DD');
+
+            const fileLink = document.getElementById('modalFile');
+            const fileName = props.file?.split('/').pop() || '파일 없음';
+            fileLink.href = props.file;
+            fileLink.setAttribute('download', '');
+            fileLink.querySelector('.file-name').textContent = fileName;
+
+            // 모달 fade-in
+            const modal = document.getElementById('eventModal');
+            modal.classList.remove('fade-out');
+            modal.style.display = 'flex';
+            requestAnimationFrame(() => {
+                modal.classList.add('fade-in');
+            });
+        },
+        eventDidMount: function (info) {
+            const iconName = info.event.extendedProps.icon;
+            if (iconName) {
+                const iconSpan = document.createElement('span');
+                iconSpan.className = 'material-symbols-outlined';
+                iconSpan.style.verticalAlign = 'middle';
+                iconSpan.style.fontSize = '16px';
+                iconSpan.style.marginRight = '4px';
+                iconSpan.textContent = iconName;
+
+                const titleEl = info.el.querySelector('.fc-event-title');
+                if (titleEl) {
+                    titleEl.prepend(iconSpan);
+                }
+            }
+        }
+    });
+
+    calendar.render();
+
+    // 범례
+    const legend = document.createElement('div');
+    legend.className = 'calendar-legend';
+    legend.innerHTML = `
+      <div class="legend-item"><span class="legend-box" style="background-color: #10b981;"></span> 기본교육</div>
+      <div class="legend-item"><span class="legend-box" style="background-color: #3b82f6;"></span> 전문교육</div>
+      <div class="legend-item"><span class="legend-box" style="background-color: #6b7280;"></span> 기타</div>
+    `;
+    document.querySelector('.fc-header-toolbar').appendChild(legend);
+
+    // 모달 닫기
+    const modal = document.getElementById('eventModal');
+    document.getElementById('closeModalBtn').onclick = () => closeModal();
+    window.onclick = function (event) {
+        if (event.target === modal) closeModal();
+    };
+
+    function closeModal() {
+        modal.classList.remove('fade-in');
+        modal.classList.add('fade-out');
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 300);
+    }
+
+    function formatParticipants(total, detail) {
+        const regionMap = {
+            daejeon: '대전',
+            sejong: '세종',
+            chungbuk: '충북',
+            chungnam: '충남',
+            fire_agency: '소방청'
+        };
+        const detailStr = Object.entries(detail || {})
+            .filter(([_, count]) => count > 0)
+            .map(([region, count]) => `${regionMap[region] || region} ${count}`)
+            .join(', ');
+        return `${total}(${detailStr})`;
+    }
+});
